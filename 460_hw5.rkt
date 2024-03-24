5bee077a39c440844d982374afc9b406769f609d
AndrewCoale <55396067+AndrewCoale@users.noreply.github.com>
2024-03-24 19:16:25 -0400

    Problem 3 done, with syntactic suite coverage

--- 460_hw5.rkt
+++ 460_hw5.rkt
@@ -40,9 +40,13 @@
                 {lambda {y}  ;
                   {+ x y}}}) ;
 
-(define times `{lambda {x}                                 ; untested, but the logic is there
-                 {lambda {y}                               ;
-                   {if0 y 0 {plus x {times x (- y 1)}}}}}) ;
+
+(define times `{lambda {x}                                         ; plait constant for problem 3
+                 {lambda {y}                                       ;
+                   {letrec {[t {lambda {n}                         ;
+                             {if0 n                                ;
+                                  0                                ;
+                                  {+ {t {+ n -1}} x}}}]} {t y}}}}) ;
 
 (module+ test
   (print-only-errors #t))
@@ -62,13 +66,13 @@
        (appE (lamE (s-exp->symbol (first bs))
                    (parse (third (s-exp->list s))))
              (parse (second bs))))]
-    [(s-exp-match? `{letrec {[SYMBOL ANY]} ANY} s)                              ; added in class {letrec {[name rhs]} body} = ,{let {[name {mk-rec {lambda {name} rhs}}]} body}
+    [(s-exp-match? `{letrec {[SYMBOL ANY]} ANY} s)                              ; added letrec to parser
      (local [(define bs (s-exp->list (first (s-exp->list (second                ;
                                                           (s-exp->list s))))))  ;
-             (define name (first bs))                                             ; check pg 34 of lecture7b.pdf
-             (define rhs (second bs))                                           ; {letrec {[name rhs]} body} = ,{let {[name {mk-rec {lambda {name} rhs}}]} body}
+             (define name (first bs))                                           ;
+             (define rhs (second bs))                                           ;
              (define body (third (s-exp->list s)))]                             ;
-       (parse `{let {[,name {,mk-rec-fun {lambda {,name} ,rhs}}]} ,body}))]                                     ; comma before mk-rec-fun
+       (parse `{let {[,name {,mk-rec-fun {lambda {,name} ,rhs}}]} ,body}))]     ;
     [(s-exp-match? `{lambda {SYMBOL} ANY} s)
      (lamE (s-exp->symbol (first (s-exp->list 
                                   (second (s-exp->list s)))))
@@ -224,12 +228,18 @@
   (test (lookup 'y (extend-env
                     (bind 'x (numV 9))
                     (extend-env (bind 'y (numV 8)) mt-env)))
-        (numV 8)))
-
-(test (interp (parse `{letrec {[f {lambda {n}
+        (numV 8))
+  (test (interp (parse `{letrec {[f {lambda {n}
                                     {if0 n
                                          0
                                          {+ {f {+ n -1}} -1}}}]}
                         {f 10}})
               mt-env)
       (numV -10))
+  (test (interp (parse (list->s-exp (list (list->s-exp (list times
+`3)) `4))) mt-env)
+        (numV 12))
+  (test (interp (parse (list->s-exp (list (list->s-exp (list plus
+`4)) `5))) mt-env)
+        (numV 9)))
+
